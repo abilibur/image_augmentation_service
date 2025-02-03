b207d9e58844b0339d00c8731d6d37ddf288e317
abilibur <rodinaekan@yandex.ru>
2025-02-03 22:46:05 +0500

    commit upload file

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from starlette.requests import Request
-
+from fastapi import FastAPI, Request, File, UploadFile
+import base64
 
 # Создаем FastAPI приложение
 app = FastAPI()
@@ -12,6 +13,25 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/")
+async def upload_image(request: Request, file: UploadFile = File(...)):
+    # Проверка типа содержимого
+    if not file.content_type.startswith("image/"):
+        return templates.TemplateResponse("index.html", {
+            "request": request,
+            "error": "Файл не является изображением"
+        })
+
+    # Чтение и кодирование изображения
+    image_data = await file.read()
+    encoded_image = base64.b64encode(image_data).decode("utf-8")
+
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "mime_type": file.content_type,
+        "encoded_image": encoded_image
+    })
 
 @app.get("/image_rotate", response_class=HTMLResponse)
 async def image_rotate(request: Request):
